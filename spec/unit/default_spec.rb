dfdd4be49797b9e925450466dc8feeb5b8d4b7b6
David Radcliffe <radcliffe.david@gmail.com>
2013-10-30 14:05:40 -0400

    initial commit

---
+++ spec/unit/default_spec.rb
@@ -0,0 +1,24 @@
+require 'chefspec'
+require 'chefspec/berkshelf'
+
+describe 'fping::default' do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  context 'in a redhat-based platform' do
+    let(:chef_run) { ChefSpec::Runner.new(:platform => 'redhat', :version  => '6.3').converge(described_recipe) }
+
+    before do
+      stub_command(/rpm -qa | grep -q '^rpmforge-release-[0-9\.-]'/).and_return(true)
+    end
+
+    it 'includes the yum::repoforge recipe' do
+      expect(chef_run).to include_recipe('yum::repoforge')
+    end
+
+  end
+
+  it 'installs the package' do
+    expect(chef_run).to install_package('fping')
+  end
+
+end
