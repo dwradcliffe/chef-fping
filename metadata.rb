235834b851ab9fb0cf6396db2f90493ee8395638
David Radcliffe <radcliffe.david@gmail.com>
2014-08-15 07:17:12 -0400

    bump version to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'radcliffe.david@gmail.com'
 license          'MIT'
 description      'Installs fping'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '1.0.0'
 
 recipe 'fping', 'Installs fping utility'
 
